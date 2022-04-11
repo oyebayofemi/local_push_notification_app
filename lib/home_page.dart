22a9455b333ab7de4a53e75d953799163afdd949
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2022-04-11 15:33:44 +0100

    building the ui

---
+++ lib/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'LOCAL PUSH NOTIFICATION',
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ButtonTheme(
+                buttonColor: Colors.blue,
+                minWidth: double.infinity,
+                height: 60,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                child: FlatButton.icon(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add_alert,
+                      size: 30,
+                    ),
+                    label: Text(
+                      'Simple Notification',
+                      style: TextStyle(fontSize: 20),
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonTheme(
+                buttonColor: Colors.green,
+                minWidth: double.infinity,
+                height: 60,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                child: FlatButton.icon(
+                    color: Colors.green,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.alarm,
+                      size: 30,
+                    ),
+                    label: Text(
+                      'Schedule Notification',
+                      style: TextStyle(fontSize: 20),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
